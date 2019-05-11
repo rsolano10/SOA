b940118b0b3e7d65a077c5e35d0b39861f8bff36
adrian039 <adrian_sa12@outlook.com>
2019-05-11 05:26:31 -0600

    code updates

--- Proyecto2/microservices/NodeServer/src/models/mysql/db.js
+++ Proyecto2/microservices/NodeServer/src/models/mysql/db.js
@@ -3,11 +3,10 @@
 const mysql = require('mysql')
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'admin',
-  password: 'prueba123',
-  database: 'ordersDB',
-  port: 3306
+  host: 'soa2019ak7.mysql.database.azure.com',
+  user: 'adminSoa@soa2019ak7',
+  password: 'Prueba123',
+  database: 'soa2019'
 });
 
 connection.connect(() => {
